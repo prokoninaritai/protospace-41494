5f11bc632c790545369338ac93ac86087c71fb27
prokoninaritai <s0a7c5h1i@gmail.com>
2024-10-19 15:26:19 +0900

    プロトタイプ編集画面インスタンス変数へ変更

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -31,9 +31,9 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)
-      redirect_to prototype_path
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
     else
       render :edit, status: :unprocessable_entity
     end
